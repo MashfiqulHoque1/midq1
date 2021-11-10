54577adcc58703b44d6e58983ff8c05ff4c98a68
MashfiqulHoque1 <1921648@iub.edu.bd>
2021-11-10 14:54:38 +0600

    fourth commit

---
+++ get started/mid1.js
@@ -0,0 +1,30 @@
+function show(){
+    var name = document.getElementById("namebox").value ;
+    var age = document.getElementById("agebox").value;
+    var sal = document.getElementById("salbox").value ;
+
+    if ((name == "") || (age== "") || (sal == "")) {
+        document.getElementById("i1").innerHTML = "Please fill up all of your information";
+    }
+
+    else if ((age > 25) && (sal>=100000)){
+        document.getElementById("i1").innerHTML = name + ". Your monthly Bonus is 20% of total Monthly Salary";
+    }
+    
+    else if((age > 30) && (sal>=200000)){
+        document.getElementById("i1").innerHTML = name + ". Your monthly Bonus is 30% of total Monthly Salary";
+    }
+
+    else if((age > 35) && (sal>=300000)){
+        document.getElementById("i1").innerHTML = name + ". Your monthly Bonus is 40% of total Monthly Salary";
+    }
+
+    else {
+        document.getElementById("i1").innerHTML = name + ". Your monthly Bonus is 10% of total Monthly Salary";
+    }
+
+
+    document.getElementById("namebox").innerHTML = '' ;
+    document.getElementById("agebox").innerHTML = '';
+    document.getElementById("salbox").innerHTML = '';
+}
